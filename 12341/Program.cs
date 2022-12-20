62492c10a0564545f43e0fac6649f36cd20b03f9
taejun <kdsgamestudio@gmail.com>
2022-12-20 20:40:40 +0900

    init

---
+++ 12341/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _12341;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ 321234/Program.cs
@@ -0,0 +1,9 @@
+namespace _321234;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
